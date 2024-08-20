f9eb94488b99570c99e863a745607a3615936473
leviathan-7 <m.grober02@yandex.ru>
2024-08-20 16:08:06 +0300

    remake gitignor

---
+++ Backend/demo/target/classes/database/initDB.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS persons
+(
+    id    BIGSERIAL PRIMARY KEY ,
+    login  VARCHAR(100) NOT NULL ,
+    password  VARCHAR(100) NOT NULL ,
+    role  VARCHAR(100) NOT NULL ,
+    sum  INT NOT NULL ,
+    first_Name  VARCHAR(100) ,
+    last_Name VARCHAR(100) ,
+    average_Name VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS cfoes
+(
+    id    BIGSERIAL PRIMARY KEY ,
+    cfo_name  VARCHAR(100) NOT NULL ,
+    sum  INT NOT NULL ,
+    basic_sum  INT NOT NULL ,
+    owner_id    BIGSERIAL ,
+    final_date  TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transactions
+(
+    id    BIGSERIAL PRIMARY KEY ,
+    t_from    BIGSERIAL,
+    t_to    BIGSERIAL,
+    owner    BIGSERIAL,
+    sum  INT NOT NULL ,
+    type  VARCHAR(100) NOT NULL ,
+    comment  VARCHAR(100) NOT NULL ,
+    datatime TIMESTAMP NOT NULL
+);
+
