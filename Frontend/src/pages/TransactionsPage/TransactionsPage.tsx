64689140bc2dec422417ce13452e98d3b063cc28
VeronaPl <vp31102003@gmail.com>
2024-08-23 01:15:19 +0300

    edit TransactionsPage

--- Frontend/src/pages/TransactionsPage/TransactionsPage.tsx
+++ Frontend/src/pages/TransactionsPage/TransactionsPage.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TransactionsPage.scss';
+import { LoginButton } from '../../shared';
 
 
 export const TransactionsPage = ():JSX.Element => {
+
+    const [modalCreate, setModalCreate] = useState<boolean>(false);
+    const [typeModal, setTypeModal] = useState<string>('');
+
     return (
         <div>
             <h1 className='MainContent__title'>История транзакций</h1>
+            <div className='MainContent__button'><LoginButton title='Создать перевод' type='button' color='green' onClick={() => {setModalCreate(true); setTypeModal('transaction')}} /></div>
         </div>
     );
 }
